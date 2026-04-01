a4e36d59cef8373532fb28cd46129921225e1fab
IDJNORIEG <33887281+IDJNORIEG@users.noreply.github.com>
2026-04-01 12:13:55 -0500

    Create messages.py for message configuration

---
+++ src/utils/messages.py
@@ -0,0 +1,9 @@
+# Messages Configuration
+
+# This file contains the configuration for messages
+
+MESSAGES = {
+    'welcome': 'Welcome to our application!',
+    'goodbye': 'Thank you for using our application!',
+    'error': 'An error has occurred. Please try again later.',
+}
